e6597cf3546a642b941df6af1a0b3aa883698f82
Jordan Chan <jordac67@gmail.com>
2015-12-16 16:27:47 -0800

    issue #1 remade allInstruments function in order to successfully pass it as a argument to the fetchData function

--- public/data.js
+++ public/data.js
@@ -16,46 +16,43 @@
 //   xhr.send();
 // }());
 
-//Fetches prices for given instrument. Also grabs E-tag.
+// Fetches prices for given instrument.
 
-// var fetchPrices = function (currencyData) {
-//   // console.log(currencyData)
-//   for (var i=0; i<currencyData.length; i++) {  
-
-//     var xhr = new XMLHttpRequest();
-//     xhr.onload = function () {
-//       var responseObject = JSON.parse(xhr.responseText);
-//       var bidPrice = JSON.parse(responseObject.body);
-//       console.log(bidPrice[i].prices[0].ask)
-//     }
-//     xhr.open('POST', '/instruments/prices');
-//     xhr.send(currencyData[i].instrument)
-//   };
-// };
+var fetchData = function (instrument) {
+  // console.log(currencyData) 
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    var responseObject = JSON.parse(xhr.responseText);
+    var bidPrice = JSON.parse(responseObject.body);
+    console.log(bidPrice);
+  }
+  xhr.open('POST', '/instruments/prices');
+  xhr.send(instrument);
+};
 
 
 
 
-function gatherData () {
+function gatherInstruments () {
   var xhr = new XMLHttpRequest;
   xhr.onload = function () {
     var currencyList = JSON.parse(xhr.responseText);
     // console.log(responseObject);
-    var allInsruments = (function () { 
-      var instrumentString = currencyList[0].instrument
+    var allInstruments = currencyList[0].instrument
+    var instrumentString = (function () {       
       for (var i=1; i<currencyList.length; i++){           
-      instrumentString += '%2C' + currencyList[i].instrument;
-      console.log(instrumentString);
-      };
+        allInstruments += '%2C' + currencyList[i].instrument;      
+      };     
     }());
-    // fetchPrices(currencies);
+    console.log(allInstruments);
+    fetchData(allInstruments);
   };
 
   xhr.open('GET', '/instruments', true);
   xhr.send();  
 };
 
-gatherData();
+gatherInstruments();
 
 // console.log(currencies);
 
