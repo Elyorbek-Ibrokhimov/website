35ac318d3ada178b03a5bab323978d2985063af0
Jordan Chan <jordac67@gmail.com>
2015-12-17 09:55:40 -0800

    issue #1 : successfully created a callback function to handle all the returned data properties. Next step is to create an object to be able to reference them.

--- public/data.js
+++ public/data.js
@@ -1,20 +1,5 @@
-//Displays the main data table that will show bid spread for major instruments
-//Will also display asking and Bid prices
-//This will require the fetchPrices function.
-//Once the page loads, run fetchPrices for major currencies and display on the table.
-//Add E-tag and AJAX to fetch for changing data. 
-  //AJAX will continually ping your server, if there is a change in the headtag then 
 
-// (function instruemnts () {
-//   var xhr = new XMLHttpRequest();
-//   xhr.onload = function () {
-//     var responseObject = xhr.responseText;
-//     var instrumentList = JSON.parse(responseObject);
-//     // console.log(instrumentList);
-//   }
-//   xhr.open('GET', '/instruments');
-//   xhr.send();
-// }());
+
 
 // Fetches prices for given instrument.
 
@@ -23,16 +8,15 @@ var fetchData = function (instrument) {
   var xhr = new XMLHttpRequest();
   xhr.onload = function () {
     var responseObject = JSON.parse(xhr.responseText);
-    var bidPrice = JSON.parse(responseObject.body);
-    console.log(bidPrice);
+    var data = (JSON.parse(responseObject.body)).prices;
+    dataProperties(data, 'ask');
+
   }
   xhr.open('POST', '/instruments/prices');
   xhr.send(instrument);
 };
 
-
-
-
+//Gathers all the insturment names to put in the url
 function gatherInstruments () {
   var xhr = new XMLHttpRequest;
   xhr.onload = function () {
@@ -44,7 +28,7 @@ function gatherInstruments () {
         allInstruments += '%2C' + currencyList[i].instrument;      
       };     
     }());
-    console.log(allInstruments);
+    // console.log(allInstruments);
     fetchData(allInstruments);
   };
 
@@ -54,7 +38,13 @@ function gatherInstruments () {
 
 gatherInstruments();
 
-// console.log(currencies);
+function dataProperties (data, property) {
+  for (var x=0; x<data.length; x++) {
+    // var propertyList = data[i].property;
+    
+    console.log(data[x][property]);
+  }
+}
 
 
 
@@ -65,18 +55,11 @@ gatherInstruments();
 
 
 
-// (function fetchPrices () {
-//   var xhr = new XMLHttpRequest();
-//   xhr.onload = function () {
-//     var responseObject = xhr.responseText;
-//     var priceList = JSON.parse(responseObject);
-//     var priceBox = document.getElementById('price-box');
-//     console.log(priceList.prices);
 
-//     var value2 = document.getElementById('value2');
-//     value2.textContent = priceList.prices[0].bid;
-//   };
-//     xhr.open('POST', '/instruments/prices');
-//     xhr.send('AUD_CAD');
-  
-// }());
+
+
+
+
+
+
+
