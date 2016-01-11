034f64fd8253bc5b7ea0a737118a8b7e0e4d9cba
Jordan Chan <jordac67@gmail.com>
2016-01-11 14:33:38 -0800

    Added sass compiler and added watch task

--- app.js
+++ app.js
@@ -2,20 +2,19 @@ var express = require('express');
     app = express();
     router = express.Router();
     instruments = require('./instruments.js');
-    sass = require('node-sass');
     path = require('path');
     sassMiddleware = require('node-sass-middleware')
 
 // console.log(client);
 
-app.use(
-   sassMiddleware({
-       src: __dirname + '/sass', 
-       dest: __dirname + '/public/stylesheets',
-       prefix:  '/stylesheets',
-       debug: true        
-   })
-); 
+// app.use(
+//    sassMiddleware({
+//        src: __dirname + '/sass', 
+//        dest: __dirname + '/public/stylesheets',
+//        prefix:  '/stylesheets',
+//        debug: true        
+//    })
+// ); 
 
 app.use(express.static('public'));
 app.use('/instruments', instruments);
--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -2,12 +2,18 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('../sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('../public/stylesheets'))
+})
 
 gulp.task('concat', function () {
   return gulp.src('../public/js/*.js')
     .pipe(concat('all.js'))
-    .pipe(gulp.dest('../public/dist'))
-    
+    .pipe(gulp.dest('../public/dist'))   
 })
 
 //need ES6 plugin
@@ -26,4 +32,9 @@ gulp.task('concat', function () {
 //     .pipe(gulp.dest('../dist'));
 // })
 
-gulp.task('default', ['concat']);
+gulp.task('watch', function () {
+  gulp.watch('../public/js/*.js', ['concat']);
+  gulp.watch('../sass/*.scss', ['sass']);
+})
+
+gulp.task('default', ['concat', 'sass', 'watch']);
