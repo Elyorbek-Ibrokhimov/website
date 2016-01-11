4f68fe16de230aaf71bfeb9572a3007daa6be464
Jordan Chan <jordac67@gmail.com>
2016-01-11 15:40:21 -0800

    Added jshint to gulp file

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -4,8 +4,15 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+var jshint = require('gulp-jshint');
 
-gulp.task('concat', () => {
+gulp.task('lint', () => {
+  gulp.src('../public/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+})
+
+gulp.task('script', () => {
   return gulp.src('../public/js/*.js')
     .pipe(babel({presets:['es2015']}))
     .pipe(concat('all.min.js'))
@@ -20,8 +27,8 @@ gulp.task('sass', () => {
 })
 
 gulp.task('watch', function () {
-  gulp.watch('../public/js/*.js', ['concat']);
+  gulp.watch('../public/js/*.js', ['lint','script']);
   gulp.watch('../sass/*.scss', ['sass']);
 })
 
-gulp.task('default', ['concat', 'sass', 'watch']);
+gulp.task('default', ['lint', 'script', 'sass', 'watch']);
