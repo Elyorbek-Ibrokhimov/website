871cfb9e58c62affa6131b3e7077aa48fa04a006
Jordan Chan <jordan@assetavenue.com>
2016-12-11 11:49:43 -0800

    removed more unecessary modules and server code

--- app/app.js
+++ app/app.js
@@ -1,14 +1,10 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
-// var mongoose = require('mongoose');
 var instruments = require('./routes/instruments.js');
-// var Currencies = require('./models/currency.js');
-
-// mongoose.connect('mongodb://localhost/currencies');
+var currencies = require('./models/currency.js');
 
 app.use(express.static('../client/public'));
-// app.use(express.static('app/dist/vendor'));
 app.use('/instruments', instruments);
 
 app.get('/', function(req,res) {
@@ -16,9 +12,7 @@ app.get('/', function(req,res) {
 });
 
 app.get('/currencies', function (req, res) {
-  Currencies.find({}).exec(function (err, currencies){
-    res.send(JSON.stringify(currencies));
-  });
+  res.send(JSON.stringify(currencies));
 });
 
 var port = process.env.PORT || 3000;
--- app/models/currency.js
+++ app/models/currency.js
@@ -1,13 +1,6 @@
 var express = require('express');
-var mongoose = require('mongoose');
 var app = express();
 var currencies = express.Router();
+var list = require('./currencies.json');
 
-var currenciesSchema = new mongoose.Schema({
-  instrument: String,
-  displayName: String
-});
-
-var Currencies = mongoose.model('Currency', currenciesSchema);
-
-module.exports = Currencies;
+module.exports = list;
