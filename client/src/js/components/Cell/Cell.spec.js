510e7c05ea49c384657a44ef66b3cefc4c787f4b
Jordan Chan <jordan@assetavenue.com>
2016-12-22 21:04:32 -0800

    missing babel rc cuasing tests to fail

--- client/src/js/components/Cell/Cell.spec.js
+++ client/src/js/components/Cell/Cell.spec.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TestUtils from 'react-addons-test-utils';
+import chai from 'chai'
 import {Cell} from './Cell.js';
 
 const data = {
@@ -11,14 +12,17 @@ const data = {
 
 describe('Cell Component', () => {
   it('should return a component representing a currency', () => {
+    console.log('CEEL ', Cell);
     const renderer = TestUtils.createRenderer();
-    renderer.render(
-      <Cell name={data.instrument} 
-            bid={data.bid} 
-            ask={data.ask} 
-            spread={data.spread}
-            filterName={data.instrument.slice(0,3)}/>)
+    renderer.render(<Cell />);
     const output = renderer.getRenderOutput();
     console.log(output);
   }) 
 })
+
+
+// name={data.instrument}
+//             bid={data.bid} 
+//             ask={data.ask} 
+//             spread={data.spread}
+//             filterName={data.instrument.slice(0,3)} 
